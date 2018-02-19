2c8f79b8d8f6771725157c019c03ca9f2aa007da
Mike Xu <mxu11290@gmail.com>
2018-02-18 22:21:50 -0500

    use cytoscape instead of react wrapper and add keyboardJS contexts by node selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
-import { ReactCytoscape } from 'react-cytoscape'
+import cytoscape from 'cytoscape'
 import keyboardJS from 'keyboardjs'
 import './App.css'
+
+const cyStyle = [
+  {
+    selector: 'node',
+    style: {
+      'content': 'data(text)'
+    }
+  },
+  {
+    selector: ':selected',
+    style: {
+      'border-width': '.1em',
+      'border-style': 'solid',
+      'border-color': 'blue',
+    }
+  }
+]
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +26,58 @@ class App extends Component {
     this.state = {
       cy: null
     }
+  }
+
+  componentDidMount() {
+    let cy = cytoscape({
+      container: document.getElementById('cy'),
+      elements: {
+        nodes: [
+          { data: { id: 'a', text: 'hello' } },
+          { data: { id: 'b', text: 'goodbye' } },
+        ],
+        edges: [
+          { data: { id: 'ab', source: 'a', target: 'b' } }
+        ]
+      },
+      style: cyStyle
+    })
+    window.cy = cy
+
+    cy.on('select', 'node', e => {
+      console.log('node selected')
+      console.log(e)
+      let selectedNodes = cy.$('node:selected').length
+      keyboardJS.setContext(selectedNodes === 1 ? 'singleNode' : 'multipleNodes')
+    })
+
+    cy.on('unselect', 'node', e => {
+      console.log('node unselected')
+      console.log(e)
+      let selectedNodes = cy.$('node:selected').length
+      if (selectedNodes === 1) keyboardJS.setContext('singleNode')
+      else if (selectedNodes === 0) keyboardJS.setContext('root')
+    })
+
+    keyboardJS.withContext('root', () => {
+      keyboardJS.bind('a', e => {
+        console.log('add node')
+      })
+    })
+
+    keyboardJS.withContext('singleNode', () => {
+      keyboardJS.bind('ctrl + e', e => {
+        console.log('edit node text')
+      })
+    })
+
+    keyboardJS.withContext('multipleNodes', () => {
+      keyboardJS.bind('ctrl + g', e => {
+        console.log('group nodes')
+      })
+    })
+
+    keyboardJS.setContext('root')
   }
 
   getElements() {
@@ -38,30 +107,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <ReactCytoscape
-          elements={this.getElements()}
-          containerID="cy"
-          style={[
-            {
-              selector: 'node',
-              style: {
-                'content': 'data(text)'
-              }
-            },
-            {
-              selector: ':selected',
-              style: {
-                'background-color': 'green'
-              }
-            }
-          ]}
-          cyRef={(cy) => { this.initCy(cy) }}
-          cytoscapeOptions={{
-            wheelSensitivity: 0.1,
-            boxSelectionEnabled: true,
-          }}
-        />
+      <div id="cy">
       </div>
     )
   }
