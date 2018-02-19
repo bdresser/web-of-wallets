238c6459716a89c6e9a9e9b1efd9adf96335d890
Mike Xu <mxu11290@gmail.com>
2018-02-18 20:56:00 -0500

    show text above node

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import { ReactCytoscape } from 'react-cytoscape'
+import keyboardJS from 'keyboardjs'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cy: null
+    }
+  }
+
   getElements() {
-    return [
-      { data: { id: 'a' } },
-      { data: { id: 'b' } },
-      {
-        data: {
-          id: 'ab',
-          source: 'a',
-          target: 'b'
-        }
-      }
-    ]
+    return {
+      nodes: [
+        { data: { id: 'a', text: 'hello' } },
+        { data: { id: 'b', text: 'goodbye' } },
+      ],
+      edges: [
+        { data: { id: 'ab', source: 'a', target: 'b' } }
+      ]
+    }
+  }
+
+  initCy(cy) {
+    if (this.state.cy) return
+    window.cy = cy
+    this.setState({
+      cy: cy
+    })
+    console.log('init cy')
+    keyboardJS.bind('a', e => {
+      console.log('a pressed')
+      console.log(e)
+    })
   }
 
   render() {
@@ -23,8 +42,24 @@ class App extends Component {
         <ReactCytoscape
           elements={this.getElements()}
           containerID="cy"
-          cyRef={(cy) => {
-            console.log(cy)
+          style={[
+            {
+              selector: 'node',
+              style: {
+                'content': 'data(text)'
+              }
+            },
+            {
+              selector: ':selected',
+              style: {
+                'background-color': 'green'
+              }
+            }
+          ]}
+          cyRef={(cy) => { this.initCy(cy) }}
+          cytoscapeOptions={{
+            wheelSensitivity: 0.1,
+            boxSelectionEnabled: true,
           }}
         />
       </div>
