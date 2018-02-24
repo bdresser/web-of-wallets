f85717c7b56b32d58b4689951efb1a49330c98bf
Mike Xu <mxu11290@gmail.com>
2018-02-24 02:11:07 -0500

    select node color and toggle collapse successors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import cytoscape from 'cytoscape'
 import keyboardJS from 'keyboardjs'
 import uniqid from 'uniqid'
+import { SketchPicker } from 'react-color'
 import './App.css'
 
 const cyStyle = [
@@ -10,16 +11,26 @@ const cyStyle = [
     style: {
       'content': 'data(text)',
       'background-color': 'grey',
+      'text-margin-y': 0,
+      'text-valign': 'center',
+      'text-halign': 'center',
+      'text-wrap': 'wrap',
+      'text-max-width': 240,
+      'width': 'label',
+      'height': 'label',
+      'shape': 'roundrectangle',
+      'padding': 10,
+      'color': 'white',
     }
   },
   {
     selector: ':selected',
     style: {
       'background-color': null,
-      'border-width': '.1em',
+      'border-width': '.3em',
       'border-style': 'solid',
-      'border-color': 'blue',
-      'line-color': 'blue',
+      'border-color': 'black',
+      'line-color': 'black',
     }
   },
   {
@@ -41,6 +52,9 @@ class App extends Component {
       selectedNodes: [],
       runningLayout: false,
       lastContext: null,
+      selectedColor: '#fff',
+      presetColors: ['#fff','#eee','#ddd','#ccc','#bbb','#aaa','#999','#888','#777'],
+      nodeStyles: {},
     }
   }
   
@@ -67,6 +81,27 @@ class App extends Component {
     this.setState({
       lastContext: null,
       showInput: false,
+    })
+  }
+
+  inputColor() {
+    const lastContext = keyboardJS.getContext()
+    keyboardJS.setContext('colorInput')
+    return new Promise((resolve, reject) => {
+      this.setState({
+        showColorPicker: true,
+        lastContext,
+        inputSubmitHandler: () => { return resolve(this.state.selectedColor) },
+        inputCancelHandler: () => { return reject('canceled by user') },
+      })
+    })
+  }
+
+  hideColor() {
+    keyboardJS.setContext(this.state.lastContext)
+    this.setState({
+      lastContext: null,
+      showColorPicker: false,
     })
   }
 
@@ -135,10 +170,32 @@ class App extends Component {
       keyboardJS.bind('s', e => {
         const json = cy.json()
         window.localStorage.setItem('mindmap', JSON.stringify(json))
+        window.localStorage.setItem('presetColors', JSON.stringify(this.state.presetColors))
+        window.localStorage.setItem('nodeStyles', JSON.stringify(this.state.nodeStyles))
+        console.group('save graph')
+        console.log(json)
+        console.log(this.state.presetColors)
+        console.log(this.state.nodeStyles)
+        console.groupEnd()
       })
       keyboardJS.bind('l', e => {
         const json = JSON.parse(window.localStorage.getItem('mindmap'))
         cy.json(json)
+        const presetColors = JSON.parse(window.localStorage.getItem('presetColors'))
+        const nodeStyles = JSON.parse(window.localStorage.getItem('nodeStyles'))
+        this.setState({
+          presetColors,
+          nodeStyles,
+        })
+
+        for (let id in nodeStyles) {
+          cy.$(`#${id}`).style(nodeStyles[id])
+        }
+        console.group('load graph')
+        console.log(json)
+        console.log(presetColors)
+        console.log(nodeStyles)
+        console.groupEnd()
       })
       keyboardJS.bind('x', null, e => {
         if (!this.state.runningLayout) {
@@ -174,6 +231,15 @@ class App extends Component {
       })
     })
 
+    const setNodeStyle = (node, style) => {
+      node.style(style)
+      const nodeStyles = {
+        ...this.state.nodeStyles,
+        [node.id()]: style
+      }
+      this.setState({nodeStyles})
+    }
+
     keyboardJS.withContext('singleNode', () => {
       keyboardJS.bind('a', null, e => {
         const selectedNode = this.state.selectedNodes[0]
@@ -208,17 +274,41 @@ class App extends Component {
           .finally(() => {this.hideInput()})
       })
       keyboardJS.bind('d', e => {
-        this.state.selectedNodes.forEach(node => {node.remove()})
+        cy.collection(this.state.selectedNodes).remove()
       })
       keyboardJS.bind(']', e => {
-        this.state.selectedNodes.forEach(node => {
-          cy.edges(`[source="${node.id()}"]`).targets().select()
-        })
+        cy.collection(this.state.selectedNodes).outgoers('node').select()
       })
       keyboardJS.bind('[', e => {
-        this.state.selectedNodes.forEach(node => {
-          cy.edges(`[target="${node.id()}"]`).sources().select()
-        })
+        cy.collection(this.state.selectedNodes).incomers('node').select()
+      })
+      keyboardJS.bind('f', null, e => {
+        const selectedNode = this.state.selectedNodes[0]
+        this.inputColor()
+          .then(color => {
+            setNodeStyle(selectedNode, { 'background-color': color, 'text-background-color': color })
+          })
+          .catch(error => {
+            console.log('set node color error:', error)
+          })
+          .finally(() => { this.hideColor() })
+      })
+      keyboardJS.bind('q', null, e => {
+        const selectedNode = this.state.selectedNodes[0]
+        const successors = selectedNode.successors('node')
+        if (successors.length > 0) {
+          cy.startBatch()
+          if (selectedNode.data('collapsedChildren')) {
+            selectedNode.data('collapsedChildren', null)
+            selectedNode.style({'content': n => n.data('text')})
+            successors.style({'display': 'element'})
+          } else {
+            selectedNode.data('collapsedChildren', successors.length)
+            selectedNode.style({'content': n => `${n.data('text')} [${successors.length}]`})
+            successors.style({'display': 'none'})
+          }
+          cy.endBatch()
+        }
       })
     })
 
@@ -249,17 +339,26 @@ class App extends Component {
         toggleEdges(this.state.selectedNodes, true)
       })
       keyboardJS.bind('d', e => {
-        this.state.selectedNodes.forEach(node => {node.remove()})
+        cy.collection(this.state.selectedNodes).remove()
       })
       keyboardJS.bind(']', e => {
-        this.state.selectedNodes.forEach(node => {
-          cy.edges(`[source="${node.id()}"]`).targets().select()
-        })
+        cy.collection(this.state.selectedNodes).outgoers('node').select()
       })
       keyboardJS.bind('[', e => {
-        this.state.selectedNodes.forEach(node => {
-          cy.edges(`[target="${node.id()}"]`).sources().select()
-        })
+        cy.collection(this.state.selectedNodes).incomers('node').select()
+      })
+      keyboardJS.bind('f', null, e => {
+        this.inputColor()
+          .then(color => {
+            const style = { 'background-color': color }
+            this.state.selectedNodes.forEach(node => {
+              setNodeStyle(node, style)
+            })
+          })
+          .catch(error => {
+            console.log('set node color canceled: ', error)
+          })
+          .finally(() => { this.hideColor() })
       })
     })
 
@@ -270,6 +369,50 @@ class App extends Component {
       keyboardJS.bind(['escape', 'ctrl + ['], e => {
         this.state.inputCancelHandler()
       })
+    })
+
+    keyboardJS.withContext('colorInput', () => {
+      keyboardJS.bind('enter', e => {
+        this.state.inputSubmitHandler()
+      })
+      keyboardJS.bind(['escape', 'ctrl + ['], e => {
+        this.state.inputCancelHandler()
+      })
+
+      const setColor = i => {
+        this.setState({
+          presetColors: [
+            ...this.state.presetColors.slice(0, i),
+            this.state.selectedColor,
+            ...this.state.presetColors.slice(i + 1)
+          ]
+        })
+      }
+      keyboardJS.bind('ctrl + 1', e => { setColor(0) })
+      keyboardJS.bind('ctrl + 2', e => { setColor(1) })
+      keyboardJS.bind('ctrl + 3', e => { setColor(2) })
+      keyboardJS.bind('ctrl + 4', e => { setColor(3) })
+      keyboardJS.bind('ctrl + 5', e => { setColor(4) })
+      keyboardJS.bind('ctrl + 6', e => { setColor(5) })
+      keyboardJS.bind('ctrl + 7', e => { setColor(6) })
+      keyboardJS.bind('ctrl + 8', e => { setColor(7) })
+      keyboardJS.bind('ctrl + 9', e => { setColor(8) })
+      keyboardJS.bind('ctrl + 0', e => { setColor(9) })
+
+      const selectPresetColor = i => {
+        this.setState({ selectedColor: this.state.presetColors[i] })
+        this.state.inputSubmitHandler()
+      }
+      keyboardJS.bind('1', e => { selectPresetColor(0) })
+      keyboardJS.bind('2', e => { selectPresetColor(1) })
+      keyboardJS.bind('3', e => { selectPresetColor(2) })
+      keyboardJS.bind('4', e => { selectPresetColor(3) })
+      keyboardJS.bind('5', e => { selectPresetColor(4) })
+      keyboardJS.bind('6', e => { selectPresetColor(5) })
+      keyboardJS.bind('7', e => { selectPresetColor(6) })
+      keyboardJS.bind('8', e => { selectPresetColor(7) })
+      keyboardJS.bind('9', e => { selectPresetColor(8) })
+      keyboardJS.bind('0', e => { selectPresetColor(9) })
     })
 
     keyboardJS.setContext('root')
@@ -296,6 +439,23 @@ class App extends Component {
               color: 'white',
               fontSize: '3em',
             }} />
+          </div>
+        }
+        { this.state.showColorPicker &&
+          <div style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            background: 'rgba(0,0,0,0.7)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <SketchPicker
+              color={this.state.selectedColor}
+              onChangeComplete={color => { this.setState({selectedColor: color.hex}) }}
+              presetColors={this.state.presetColors}
+            />
           </div>
         }
       </div>
